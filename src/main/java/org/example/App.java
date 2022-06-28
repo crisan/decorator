070b592b6557ee668c18d6dc811ceb25309a8c70
PREVIRED\cfarias <cfarias@previred.com>
2022-06-28 15:24:15 -0400

    Se agrega comentario 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args ) {
         // Comentario 1
         // Comentario 2
+        // Comentario 3
         System.out.println( "Hello World!" );
     }
 }
