899fd82bd50c8d2ee47d7ad257425b86bd95c37c
PREVIRED\cfarias <cfarias@previred.com>
2022-06-28 15:42:17 -0400

    se eliminan comentarios

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,8 +7,6 @@ package org.example;
 public class App 
 {
     public static void main( String[] args ) {
-        // Comentario 1
-        // Comentario 2
         System.out.println( "Hello World!" );
     }
 }
