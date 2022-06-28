11e7095b0d7a700b5a57a86316ccb147b6529b96
PREVIRED\cfarias <cfarias@previred.com>
2022-06-28 11:25:02 -0400

    Se agrega valor 1 al comentario

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ package org.example;
 public class App 
 {
     public static void main( String[] args ) {
-        // Comentario
+        // Comentario 1
         System.out.println( "Hello World!" );
     }
 }
